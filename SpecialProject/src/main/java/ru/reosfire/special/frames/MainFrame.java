7f6f28e6883073efcb9f739822aa26f062576563
reosfire <2.com9761721@gmail.com>
2023-11-29 01:52:37 +0300

    probably done

--- SpecialProject/src/main/java/ru/reosfire/special/frames/MainFrame.java
+++ SpecialProject/src/main/java/ru/reosfire/special/frames/MainFrame.java
@@ -13,10 +13,13 @@ public class MainFrame extends JFrame {
 
     MoleculeConstructor moleculeConstructor = new MoleculeConstructor();
 
-    File saveFile = new File("./aaa.mol");
+    File saveFile;
 
     public MainFrame() throws HeadlessException {
         setJMenuBar(createMenuBar());
+
+        moleculeConstructor.registerKeyboardAction(e -> save(), KeyStroke.getKeyStroke("control S"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+        moleculeConstructor.registerKeyboardAction(e -> load(), KeyStroke.getKeyStroke("control O"), JComponent.WHEN_IN_FOCUSED_WINDOW);
 
         setSize(WIDTH, HEIGHT);
         add(moleculeConstructor);
@@ -29,19 +32,20 @@ public class MainFrame extends JFrame {
         JMenuItem saveItem = new JMenuItem(new AbstractAction("Save") {
             @Override
             public void actionPerformed(ActionEvent e) {
-                moleculeConstructor.saveToFile(saveFile);
+                save();
             }
         });
         JMenuItem saveAsItem = new JMenuItem(new AbstractAction("Save as") {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                selectFile("select save file name", FileDialog.SAVE);
+                moleculeConstructor.saveToFile(saveFile);
             }
         });
         JMenuItem loadItem = new JMenuItem(new AbstractAction("Load") {
             @Override
             public void actionPerformed(ActionEvent e) {
-                moleculeConstructor.restoreFromFile(saveFile);
+                load();
             }
         });
 
@@ -54,7 +58,46 @@ public class MainFrame extends JFrame {
         return menuBar;
     }
 
-    private void saveAs() {
+    private void save() {
+        if (saveFile == null) selectFile("select save file name", FileDialog.SAVE);
+        moleculeConstructor.saveToFile(saveFile);
+    }
+    private void load() {
+        selectFile("select file to open", FileDialog.LOAD);
+        moleculeConstructor.restoreFromFile(saveFile);
+    }
 
+    private void selectFile(String title, int mode) {
+        while (true) {
+            FileDialog fileChooser = new FileDialog(this, title, mode);
+
+            fileChooser.setMultipleMode(false);
+
+//        String directory = fileChooser.getDirectory();
+//
+//        File[] files = new File(directory).listFiles();
+//        TreeSet<Integer> numbers = new TreeSet<>();
+//        if (files != null) {
+//            RegularExpression fileMatcher = new RegularExpression("untitled(\\d*)\\.mol");
+//            for (File file : files) {
+//                Match match = new Match();
+//                boolean matches = fileMatcher.matches(file.getName(), match);
+//
+//                if (matches) numbers.add(Integer.parseInt(match.getCapturedText(0)));
+//            }
+//        }
+
+            int mex = 0;
+
+//        while (numbers.contains(mex)) mex++;
+
+            fileChooser.setFile("default" + mex + ".mol");
+            fileChooser.setVisible(true);
+
+            if (fileChooser.getFiles().length == 0) continue;
+
+            saveFile = fileChooser.getFiles()[0];
+            break;
+        }
     }
 }
